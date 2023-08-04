734a0e87fb7e0dca67184103e95efa32e1b02990
Lisec <jan.lisec@bam.de>
2023-08-04 12:43:28 +0200

    renv IsoCor package updated

--- app.R
+++ app.R
@@ -1,9 +1,11 @@
 # Launch the shinyApp (Do not remove this comment)
+
 # To deploy: 
-# rsconnect::deployApp(appDir = "C:/Users/jlisec/Documents/Rpackages/Rpackage_IsoCor/isocor", appName = c("test","IsoCor","Praktikum")[3], forceUpdate = TRUE, account = "jali")
-# To check for errors use: 
+# rsconnect::deployApp(appDir = "C:/Users/jlisec/Documents/Rpackages/Rpackage_IsoCor/isocor", appName = c("test","IsoCor","Praktikum")[2], forceUpdate = TRUE, account = "jali")
+# To upload an Info page only use the app in: c:\Users\jlisec\Documents\Rpackages\ShinyApps\ShinyInfoPage\
+
+# To check for errors use the blue button on top of this file or: 
 # rsconnect::showLogs(appName = "test", account = "jali")
-# Or use the blue button on top of this file
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options("golem.app.prod" = TRUE)
